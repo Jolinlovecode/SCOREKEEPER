597db57a27dd26b19408d9c913f78854b0a92b67
Hongling Zhang <honglingzhang@Honglings-MacBook-Pro.local>
2022-06-29 23:35:57 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector("#p2Button");
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
+
+let p1Score = 0;
